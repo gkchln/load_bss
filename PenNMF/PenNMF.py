164c58c4879036311125dff2cf661ef771ca223c
gkchln <121186725+gkchln@users.noreply.github.com>
2024-09-13 09:48:58 +0200

    Generalise the PenNMF to the double constraint case

--- PenNMF/PenNMF.py
+++ PenNMF/PenNMF.py
@@ -9,19 +9,23 @@ from utils import ConvergenceWarning, NotFittedError, squared_norm
 
 EPSILON = np.finfo(np.float32).eps
 
-def _multiplicative_update_s(X, C, S):
+def _multiplicative_update_s(X, C, S, Z, D, E, beta):
     """Performs the multiplicative update of matrix S
 
     Args:
         X (2D array): Matrix X of shape n x p
         C (2D array): Matrix C of shape n x K
         S (2D array): Matrix S of shape K x p
+        Z (2D array): Matrix Z of shape l x q
+        D (2D array): Matrix D of shape p x q
+        E (2D array): Matrix E of shape l x K
+        beta (float): regularisation parameter
 
     Returns:
         2D array: Updated matrix S
     """
-    numerator = np.dot(C.T, X)
-    denominator = np.linalg.multi_dot([C.T, C, S])
+    numerator = np.dot(C.T, X) + beta * np.linalg.multi_dot([E.T, Z, D.T])
+    denominator = np.linalg.multi_dot([C.T, C, S]) + beta * np.linalg.multi_dot([E.T, E, S, D, D.T])
     denominator[denominator == 0] = EPSILON
     update_factor = numerator / denominator
     S *= update_factor
@@ -64,7 +68,7 @@ def _loss_nmf(X, C, S):
     """
     return squared_norm(X - np.dot(C, S))
 
-def _loss_constraint(Y, B, C, A):
+def _loss_constraint_c(Y, B, C, A):
     """Compute the second part of the loss corresponding to the constraint on C
 
     Args:
@@ -78,8 +82,22 @@ def _loss_constraint(Y, B, C, A):
     """
     return squared_norm(Y - multi_dot([B, C, A]))
 
-def _loss(X, C, S, Y, A, B, alpha):
-    """Compute the global loss as the sum of the loss related to the NMF approximation and the loss related to the constraint on C
+def _loss_constraint_s(Z, E, S, D):
+    """Compute the third part of the loss corresponding to the constraint on S
+
+    Args:
+        Z (2D array): Matrix Z of shape l x q
+        E (2D array): Matrix B of shape l x K
+        S (2D array): Matrix C of shape K x p
+        D (2D array): Matrix A of shape p x q
+
+    Returns:
+        float: loss value ||Z - ESD||^2
+    """
+    return squared_norm(Z - multi_dot([E, S, D]))
+
+def _loss(X, C, S, Y, A, B, Z, D, E, alpha, beta):
+    """Compute the global loss as the sum of the loss related to the NMF approximation and the loss related to the constraints on C and S
 
     Args:
         X (2D array): Matrix X of shape n x p
@@ -88,14 +106,18 @@ def _loss(X, C, S, Y, A, B, alpha):
         Y (2D array): Matrix Y of shape m x g
         A (2D array): Matrix A of shape K x g
         B (2D array): Matrix B of shape m x n
-        alpha (float): regularisation parameter
+        Z (2D array): Matrix Z of shape l x q
+        D (2D array): Matrix D of shape p x q
+        E (2D array): Matrix E of shape l x K
+        alpha (float): regularisation parameter for C
+        beta (float): regularisation parameter for S
     
     Returns:
         float: loss value ||X - CS||^2 + alpha * ||Y - BCA||^2
     """
-    return _loss_nmf(X, C, S) + alpha * _loss_constraint(Y, B, C, A)
+    return _loss_nmf(X, C, S) + alpha * _loss_constraint_c(Y, B, C, A) + beta * _loss_constraint_s(Z, E, S, D)
 
-def _fit_transform(X, C_init, S_init, Y, A, B, alpha, max_iter, tol, fit=True, return_loss_sequence=True, verbose=0):
+def _fit_transform(X, C_init, S_init, Y, A, B, Z, D, E, alpha, beta, max_iter, tol, fit=True, return_loss_sequence=True, verbose=0):
         """Fit the penalized multiplicative update procedure to provided matrices or
         compute the concentrations of fitted sources S for a new input matrix X (in this
         case we take the loss without the constraint on C)
@@ -107,11 +129,15 @@ def _fit_transform(X, C_init, S_init, Y, A, B, alpha, max_iter, tol, fit=True, r
             Y (2D array): Matrix Y of shape m x g
             A (2D array): Matrix A of shape K x g
             B (2D array): Matrix B of shape m x n
-            alpha (float): Regularisation parameter
+            Z (2D array): Matrix Z of shape l x q
+            D (2D array): Matrix D of shape p x q
+            E (2D array): Matrix E of shape l x K
+            alpha (float): Regularisation parameter for C
+            beta (float): regularisation parameter for S
             max_iter (int): Maximum number of iterations
             tol (float): Tolerance for convergence
             fit (bool): Whether to fit or to transform only (in case of tranform, S
-                        will not be updated and the constraint term in the loss will
+                        will not be updated and the constraint terms in the loss will
                         not be considered). Default it True.
             verbose (int): Verbose level
 
@@ -124,12 +150,14 @@ def _fit_transform(X, C_init, S_init, Y, A, B, alpha, max_iter, tol, fit=True, r
         start_time = time.time()
 
         loss_nmf = _loss_nmf(X, C_init, S_init)
-        loss_constraint = _loss_constraint(Y, B, C_init, A)
+        loss_constraint_c = _loss_constraint_c(Y, B, C_init, A)
+        loss_constraint_s = _loss_constraint_s(Z, E, S_init, D)
         # We store the losses in case we want to return them for diagnostic
         losses_nmf = [loss_nmf]
-        losses_constraint  = [loss_constraint]
+        losses_constraint_c  = [loss_constraint_c]
+        losses_constraint_s  = [loss_constraint_s]
 
-        error_at_init = loss_nmf + alpha * loss_constraint
+        error_at_init = loss_nmf + alpha * loss_constraint_c + beta * loss_constraint_s
         previous_error = error_at_init
 
         C, S = C_init.copy(), S_init.copy()
@@ -137,15 +165,17 @@ def _fit_transform(X, C_init, S_init, Y, A, B, alpha, max_iter, tol, fit=True, r
         for n_iter in range(1, max_iter + 1):
             C = _multiplicative_update_c(X, C, S, Y, A, B, alpha)
             if fit:
-                S = _multiplicative_update_s(X, C, S)
+                S = _multiplicative_update_s(X, C, S, Z, D, E, beta)
 
             if tol > 0 and n_iter % 10 == 0:
                 loss_nmf = _loss_nmf(X, C, S)
-                loss_constraint = _loss_constraint(Y, B, C, A)
+                loss_constraint_c = _loss_constraint_c(Y, B, C, A)
+                loss_constraint_s = _loss_constraint_s(Z, E, S, D)
                 losses_nmf.append(loss_nmf)
-                losses_constraint.append(loss_constraint)
+                losses_constraint_c.append(loss_constraint_c)
+                losses_constraint_s.append(loss_constraint_s)
 
-                error = loss_nmf + alpha * loss_constraint
+                error = loss_nmf + alpha * loss_constraint_c + beta * loss_constraint_s
 
                 if verbose:
                     iter_time = time.time()
@@ -172,7 +202,7 @@ def _fit_transform(X, C_init, S_init, Y, A, B, alpha, max_iter, tol, fit=True, r
                 ConvergenceWarning,
             )
         if return_loss_sequence:
-            return C, S, n_iter, losses_nmf, losses_constraint
+            return C, S, n_iter, losses_nmf, losses_constraint_c, losses_constraint_s
         else:
             return C, S, n_iter
 
@@ -180,17 +210,18 @@ class PenNMF:
     """
     Penalized Non-negative Matrix Factorization:
 
-        C,S = argmin ||X-CS||^2 + alpha * ||BCA - Y||^2
+        C,S = argmin ||X-CS||^2 + alpha * ||BCA - Y||^2 + beta * ||ESD - Z||^2
 
     """
-    def __init__(self, n_components, alpha, tol=1e-5, max_iter=10000, verbose=0):
+    def __init__(self, n_components, alpha, beta, tol=1e-5, max_iter=10000, verbose=0):
         self.n_components = n_components
         self.alpha = alpha
+        self.beta = beta
         self.tol = tol
         self.max_iter = max_iter
         self.verbose = verbose
 
-    def fit_transform(self, X, C_init, S_init, Y, A, B):
+    def fit_transform(self, X, C_init, S_init, Y, A, B, Z, D, E):
         """Fit the penalized multiplicative update procedure to provided matrices
 
         Args:
@@ -200,17 +231,22 @@ class PenNMF:
             Y (2D array): Matrix Y of shape m x g
             A (2D array): Matrix A of shape K x g
             B (2D array): Matrix B of shape m x n
+            Z (2D array): Matrix Z of shape l x q
+            D (2D array): Matrix D of shape p x q
+            E (2D array): Matrix E of shape l x K
 
         Returns:
             C (2D array): Matrix C of shape n x K. Fitted concentrations.
         """
-        C, S, n_iter, losses_nmf, losses_constraint = _fit_transform(X, C_init, S_init, Y, A, B, self.alpha, self.max_iter, self.tol, verbose=self.verbose)
+        C, S, n_iter, losses_nmf, losses_constraint_c, losses_constraint_s = _fit_transform(X, C_init, S_init, Y, A, B, Z, D, E, self.alpha, self.beta, self.max_iter, self.tol,
+                                                                                            verbose=self.verbose)
 
         self.n_components_ = S.shape[0]
         self.components_ = S
         self.n_iter_ = n_iter
         self.losses_nmf_ = losses_nmf
-        self.losses_constraint_ = losses_constraint
+        self.losses_constraint_c_ = losses_constraint_c
+        self.losses_constraint_s_ = losses_constraint_s
 
         return C
     
@@ -227,17 +263,21 @@ class PenNMF:
                                  "appropriate arguments before using transform.")
         
         # In this case we just want to estimate the concentrations
-        # we do not consider the contraint on C
+        # we do not consider the contraint on C and S
         alpha = 0
-        # We just set a null value of the matrices for compatibility in the code
+        beta = 0
+        # We just set a null value of the matrices appearing in the constraint terms for compatibility in the code
         Y = np.zeros((1,1))
         A = np.zeros((self.n_components_, 1))
-        B = np.zeros((1, len(X)))
+        B = np.zeros((1, X.shape[0]))
+        Z = np.zeros((1,1))
+        D = np.zeros((X.shape[1], 1))
+        E = np.zeros((1, self.n_components_))
 
         if C_init is None:
-            C_init = np.random.rand(len(X), self.n_components_)
+            C_init = np.random.rand(X.shape[0], self.n_components_)
             C_init = C_init / C_init.sum(axis=1, keepdims=True) # Normalize to "project on simplex"
         
-        C, *_ = _fit_transform(X, C_init, self.components_, Y, A, B, alpha, self.max_iter, self.tol, fit=False, verbose=self.verbose)
+        C, *_ = _fit_transform(X, C_init, self.components_, Y, A, B, Z, D, E, alpha, beta, self.max_iter, self.tol, fit=False, verbose=self.verbose)
 
         return C
